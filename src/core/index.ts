fc67875a243c11a0dcb62a79bb115efb7e22ee18
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-12 14:19:57 +0200

    feat(core): expose core public surface

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+export * from './cards.js';
+export * from './rules.js';
+export * from './model.js';
+export * from './legality.js';
+export * from './reducer.js';
+export * from './invariants.js';
+export * from './bots.js';
