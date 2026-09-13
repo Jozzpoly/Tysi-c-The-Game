32d501c80ae2340155694ca08dacfa1ccedb7e41
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-14 00:18:17 +0200

    Run 02: load positive tactile feedback layer

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './visual-language-02.css';
 import './visual-language-03.css';
 import './tactile-hand.css';
 import './tactile-hand-fit.css';
+import './tactile-positive-feedback.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
