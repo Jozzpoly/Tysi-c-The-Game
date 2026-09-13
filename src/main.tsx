2b6797e1986ba8466a7cf91b8b2e58f2855a8b84
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-13 23:40:42 +0200

    Run 02: load tactile hand layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './touch.css';
 import './visual-language.css';
 import './visual-language-02.css';
 import './visual-language-03.css';
+import './tactile-hand.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
