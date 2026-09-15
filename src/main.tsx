deddaad97ff24c08c64595f8075804d2ccbf2498
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-15 15:33:33 +0200

    chore(css): remove legacy touch contract import

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import './visual-language-02.css';
 import './visual-language-03.css';
 import './run04-scene-compression.css';
 import './run04-tactile-hand.css';
-import './run04-touch-contract.css';
 import './run04-trick-lifecycle.css';
 import './run04-friend-demo.css';
 import './run04-friend-feedback.css';
